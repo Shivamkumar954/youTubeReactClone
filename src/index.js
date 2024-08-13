1da40af36366e64609602f90dbb86d0d70a2c93c
Shivamkumar9549 <145294955+Shivamkumar9549@users.noreply.github.com>
2024-08-14 03:10:48 +0530

    youtube clone react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));//fetching from publiv/index.html
+
+root.render(<App />);
